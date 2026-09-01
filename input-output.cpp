38974e5a61bf3baaf766298696af97ce5f1d6530
jangiryogi <yjangir599@gmail.com>
2026-09-01 22:53:23 +0530

    Implement multiplication of three numbers

---
+++ input-output.cpp
@@ -0,0 +1,19 @@
+//take input of 3 no and there multiplication//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    int M;
+    cout<< "enter the no" << endl;
+    cin >> a >> b >> c ;
+
+    M = a * b * c;
+    cout << "M =" << M << endl;
+    cout << "this is the answer" << endl;
+
+    return 0;
+
+}
