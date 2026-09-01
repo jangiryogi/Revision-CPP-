bc169394f8d21d1fd1a31c17cd35d935cb648290
jangiryogi <yjangir599@gmail.com>
2026-09-01 23:03:43 +0530

    Add program to calculate average of two numbers

---
+++ avg of two no.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a , b ;
+    float avg ;
+    int sum;
+     
+    cout<< "ENTER THE NUMBER"<< endl;
+    cin >> a >> b ;
+    sum = a + b ;
+
+
+     avg = sum / 2 ;
+     cout << " avg = " << avg << endl;
+
+
+    return 0;
+
+}
