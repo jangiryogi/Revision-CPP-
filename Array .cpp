b3f13174177f5b3077dbece4a99d5c85f90877ad
jangiryogi <yjangir599@gmail.com>
2026-09-09 23:49:38 +0530

    Implement pair finding in an array

---
+++ Array .cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {2, 7, 11, 15};
+    int n = 4;
+    int target = 9;
+    bool found = false;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] + arr[j] == target) {
+                cout << "Pair found: " << arr[i] << " + " << arr[j] << " = " << target << endl;
+                cout << "Indices: " << i << ", " << j << endl;
+                found = true;
+            }
+        }
+    }
+
+    if (!found) {
+        cout << "No pair found." << endl;
+    }
+
+    return 0;
+}
