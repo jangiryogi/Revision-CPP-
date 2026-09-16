68562d6ffaee11d327ea8767d73ddda834a37c98
jangiryogi <yjangir599@gmail.com>
2026-09-16 22:53:59 +0530

    Implement main function for median of two sorted arrays
    Added main function to demonstrate median calculation.

--- .cpp
+++ .cpp
@@ -1,3 +1,12 @@
-if{
+Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
 
-es
+
+  #include <iostream>
+int main() {
+    Solution s;
+    vector<int> a = {1, 3}, b = {2};
+    cout << s.findMedianSortedArrays(a, b) << endl;   // 2
+
+    vector<int> c = {1, 2}, d = {3, 4};
+    cout << s.findMedianSortedArrays(c, d) << endl;   // 2.5
+}
