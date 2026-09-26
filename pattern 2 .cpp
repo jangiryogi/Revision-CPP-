8c2e8b29a75c682e09153af60f11ab502c24cb05
jangiryogi <yjangir599@gmail.com>
2026-09-26 22:43:34 +0530

    Add palindrome number pyramid program in C++
    This program prompts the user for the number of rows and prints a palindrome number pyramid based on the input.

---
+++ pattern 2 .cpp
@@ -0,0 +1,21 @@
+Question: Write a C++ program that takes an integer n and prints a palindrome number pyramid.
+
+  #include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of rows: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n - i; j++)
+            cout << " ";
+        for (int k = 1; k <= i; k++)
+            cout << k;
+        for (int k = i - 1; k >= 1; k--)
+            cout << k;
+        cout << endl;
+    }
+    return 0;
+}
